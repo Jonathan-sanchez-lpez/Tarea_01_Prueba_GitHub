78493d8bee40168d96192ec06dc26d0ddb42d2e9
Jonathan-sanchez-lpez <79613951+Jonathan-sanchez-lpez@users.noreply.github.com>
2021-02-25 18:50:18 -0600

    Add files via upload
    Tarea 1

---
+++ 2AV4 T1.c
@@ -0,0 +1,10 @@
+//2AV4 SANCHEZ LOPEZ JONATHAN
+// Tarea 1
+#include<stdio.h>
+void main()
+{
+    char nom [20];
+    printf("Escribe tu nbombre \n");
+    scanf ("%s",&nom);
+    printf ("Hola:%s",nom);
+}
